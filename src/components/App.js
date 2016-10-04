a66d9fc8bf75fc5d36b2601c1afd4d069ce51b00
Cassie Koomjian <schmitzc@gmail.com>
2016-10-04 11:16:44 -0700

    Setup firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 
 import { Header } from './common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyD_Cdt1r1Q6utUQL4afg-vq530-S6mBOSA',
+      authDomain: 'reactnative-auth-3fc74.firebaseapp.com',
+      databaseURL: 'https://reactnative-auth-3fc74.firebaseio.com',
+      storageBucket: 'reactnative-auth-3fc74.appspot.com',
+      messagingSenderId: '1084024094014'
+    });
+  }
+
   render() {
     return (
       <View>
