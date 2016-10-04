21582522a7a5a7c1447ff0e0bcb0f4a7afb91378
Cassie Koomjian <schmitzc@gmail.com>
2016-10-04 16:01:31 -0700

    Only show login form when logged out

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 
-import { Header } from './components/common';
+import { Button, Header, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyD_Cdt1r1Q6utUQL4afg-vq530-S6mBOSA',
@@ -14,13 +16,38 @@ class App extends Component {
       storageBucket: 'reactnative-auth-3fc74.appspot.com',
       messagingSenderId: '1084024094014'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button>
+            Log Out
+          </Button>
+        );
+
+      case false:
+        return <LoginForm />;
+
+      default:
+        return <Spinner size='large' />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
